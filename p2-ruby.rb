6a7b0865bd6ca33c9e532a31d4bad421162489ab
James Haley <james@wrapp.com>
2014-01-31 10:03:22 -0800

    Start problem 3

--- p2-ruby.rb
+++ p2-ruby.rb
@@ -9,4 +9,23 @@ def fibs(max)
   sum
 end
 
-puts fibs(1_000_000)
+s = Time.now
+10000.times{ fibs(1_000_000) }
+puts Time.now - s
+
+
+def fibs2
+  i = 0
+  sum = 0
+  seq = [0, 1]
+  while sum < 4_000_000
+    seq << seq[-1] + seq[-2]
+    sum += seq[-1] if seq[-1].even?
+    i += 1
+  end
+  sum
+end
+
+s = Time.now
+10000.times{ fibs2() }
+puts Time.now - s
