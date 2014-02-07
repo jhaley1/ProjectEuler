f4820d4695221d1ab97164e25901124dcd649f6c
James Haley <james@wrapp.com>
2014-02-07 13:45:11 -0800

    Solution to problem 7

---
+++ p6-ruby.rb
@@ -0,0 +1,18 @@
+def sum_of_squares
+  (1..100).inject do |sum, num|
+    sum += num ** 2
+    sum
+  end
+end
+
+def square_of_sum
+  (1..100).inject(:+) ** 2
+end
+
+def difference
+   square_of_sum - sum_of_squares
+end
+
+s = Time.now
+1000.times { difference }
+puts Time.now - s
