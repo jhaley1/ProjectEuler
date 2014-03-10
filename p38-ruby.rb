3e071cfedfba1bd28c3b67082858fa0ea8dea5fb
James Haley <james@wrapp.com>
2014-03-10 08:55:11 -0700

    Start problem 38

---
+++ p38-ruby.rb
@@ -0,0 +1,31 @@
+# Take the number 192 and multiply it by each of 1, 2, and 3:
+#
+# 192 × 1 = 192
+# 192 × 2 = 384
+# 192 × 3 = 576
+# By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+#
+# The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+#
+# What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
+
+def pandigital_multiples
+  arr = []
+
+  (10..999).each do |num|
+    (1..9).each do |a|
+      temp = []
+      a.times do
+        p = num * a
+        temp << p
+      end
+
+      arr << temp[0] if temp[0].to_s.length == 9
+    end
+  end
+
+  p arr
+  arr.max
+end
+
+puts pandigital_multiples
