42ba7bbed7512eead68c01b95e6a6e4822bc6e48
James Haley <jlhaley1@gmail.com>
2014-03-23 17:37:20 -0700

    Add first implementation of p1 for Julia

---
+++ julia/p1.jl
@@ -0,0 +1,24 @@
+function mults()
+  m = [mod(i, 3)==0 ? i : mod(i,5)==0 ? i : 0 for i=1:999]
+  sum(m)
+end
+
+t1 = time()
+i = 0
+while i < 10000
+  mults()
+  i += 1
+end
+t2 = time()
+println("elapsed time: ", t2 - t1, " seconds")
+
+
+t1 = time()
+i = 0
+while i < 10000
+  m = [mod(i, 3)==0 ? i : mod(i,5)==0 ? i : 0 for i=1:999]
+  sum(m)
+  i += 1
+end
+t2 = time()
+println("elapsed time: ", t2 - t1, " seconds")
